44654bd1fb3dcadcc036aa5712f04062099e5782
Matic Leva <vertical3life@gmail.com>
2020-04-13 10:32:18 +0200

    Fix race condition

--- src/client/v2/encounter/view/GM/ResolveResult.tsx
+++ src/client/v2/encounter/view/GM/ResolveResult.tsx
@@ -1,4 +1,4 @@
-import {FC, useEffect, useRef} from 'react';
+import {FC, useEffect} from 'react';
 import {observer} from 'mobx-react';
 import {Encounter} from '../../../../../server/model/encounter';
 import {filter, flatMap, isEmpty, isNil, map, negate, some} from 'lodash';
@@ -6,7 +6,7 @@ import {useLoader} from '../../../helpers/Store';
 import {findMonster} from '../../../types/bestiary';
 import {ResolveResult as ResolveResultBody} from '../../../../../server/service/log';
 import {roll} from '../../../../common/roll';
-import {fakeRequest, useBackend} from '../../../helpers/BackendProvider';
+import {useBackend} from '../../../helpers/BackendProvider';
 import {Log} from '../../../../../server/model/log';
 
 type Props = {
@@ -16,14 +16,14 @@ type Props = {
 export const ResolveResult: FC<Props> = observer(({encounter}) => {
     const {api} = useBackend();
     const _loader = useLoader<any[]>();
-    const promise = useRef<Promise<any> | undefined>();
 
     const logs = filter(encounter.logs, l =>
         l.stage === 'WaitingOnResult' &&
         some(map(l.target, (f, index) => ({target: f, success: l.success[index]})),
             ({target, success}) => target.type === 'npc' && isNil(success)));
     useEffect(() => {
-        if (!!promise.current || isEmpty(logs))
+        const isLoading = some(Object.values(_loader.loading), Boolean);
+        if (isLoading || isEmpty(logs))
             return;
         type Result = { log: Log } & ResolveResultBody;
         const results = filter(flatMap(logs, (log) => map(log.target, feature => {
@@ -46,13 +46,12 @@ export const ResolveResult: FC<Props> = observer(({encounter}) => {
                 } as Result;
             }
         })), negate(isNil)) as Result[];
-        const requests = map(results, ({log, ...body}) =>
-            api.put(`/log/${log.id}/resolve-result`, body)).map(p => p.catch(e => e));
-        const request = fakeRequest(() => requests);
-        promise.current = _loader.fetchAsync(request, encounter.id)
-            .catch(e => e)
-            .then(() => promise.current = undefined);
-
+        (async function () {
+            for (const {log, ...body} of results)
+                await _loader.fetchAsync(
+                    api.put(`/log/${log.id}/resolve-result`, body),
+                    `${log.id}/${body.featureId}`).catch(e => e);
+        })();
     }, [_loader, api, encounter.id, logs]);
     return null;
 });
