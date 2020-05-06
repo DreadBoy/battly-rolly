a60bf4f0011c72c3c66db645b6c9a076651f4ba5
Matic Leva <vertical3life@gmail.com>
2020-05-06 19:40:52 +0200

    Implement new layout of menu bar

--- src/client/v2/Layout.tsx
+++ src/client/v2/Layout.tsx
@@ -1,23 +1,51 @@
-import React, {FC, useEffect, useState} from 'react';
-import {Container, Icon, Menu} from 'semantic-ui-react';
+import React, {FC, useCallback, useEffect, useState} from 'react';
+import {Container, Dropdown, Icon, Menu, Responsive} from 'semantic-ui-react';
 import {createUseStyles} from 'react-jss';
 import icon from '../../assets/dice-twenty-faces-twenty.svg';
-import {Link} from 'react-router-dom';
+import {Link, Redirect} from 'react-router-dom';
 import {usePlayerId} from './helpers/PlayerId';
 import {useBackend} from './helpers/BackendProvider';
 import {Encounter} from '../../server/model/encounter';
 import {root} from './v2';
+import {useLoader} from './helpers/Store';
 
 const useStyles = createUseStyles({
     grid: {
         height: '100vh',
         width: '100vw',
     },
+    link: {
+        'a&': {
+            color: 'rgba(0,0,0,.87)',
+            textDecoration: 'none',
+        },
+    },
+    logo: {
+        display: 'block',
+        width: '1.6em',
+    },
+    flex: {
+        flex: 1,
+    },
+    border: {
+        top: 0,
+        right: 0,
+        height: '100%',
+        width: 1,
+        background: 'rgba(34,36,38,.1)',
+    },
+    fakeItem: {
+        width: 44.25,
+    },
+    menu: {
+        width: '100vw',
+    }
 });
 
 export const Layout: FC = ({children}) => {
     const classes = useStyles();
-    const {id} = usePlayerId();
+    const {id, onLogin} = usePlayerId();
+    const {api} = useBackend();
     const {socket} = useBackend();
     const [encounter, setEncounter] = useState<Encounter | null>(null);
 
@@ -32,13 +60,23 @@ export const Layout: FC = ({children}) => {
         };
     }, [socket]);
 
+    const loader = useLoader();
+    const logout = useCallback(() => {
+        loader.fetchAsync(api.delete('/auth'), 'logout')
+            .then(() => onLogin(null))
+            .catch(e => e);
+    }, [api, loader, onLogin]);
+
+    if (!!loader.data['logout'])
+        return <Redirect to={root('/login')}/>
+
     return (
         <div className={classes.grid}>
-            <Menu>
+            <Responsive minWidth={640} as={Menu}>
                 <Container>
-                    <Link to={root()} className={'header item'}>
+                    <Menu.Item header>
                         <img src={icon} alt={'icon'}/>
-                    </Link>
+                    </Menu.Item>
                     {encounter && (
                         <Menu.Item>
                             <span>
@@ -49,14 +87,56 @@ export const Layout: FC = ({children}) => {
                             </span>
                         </Menu.Item>
                     )}
-                    <Menu.Item position={'right'}>
-                        {/*TODO Use some logic to create this link*/}
-                        <Link to={root(`/user/${id}/edit`)}>
-                            <Icon name='user outline'/>
-                        </Link>
+                    <Menu.Item>
+                        <Link className={classes.link} to={root('/campaign')}>Campaigns</Link>
                     </Menu.Item>
+                    <Menu.Item>
+                        <Link className={classes.link} to={root('/monsters')}>Monsters</Link>
+                    </Menu.Item>
+                    <Menu.Menu position={'right'}>
+                        <Dropdown icon={'user outline'} item>
+                            <Dropdown.Menu>
+                                <Dropdown.Item>
+                                    <Link className={classes.link} to={root(`/user/${id}`)}>
+                                        Profile
+                                    </Link>
+                                </Dropdown.Item>
+                                <Dropdown.Item onClick={logout}>
+                                    Logout
+                                </Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown>
+                    </Menu.Menu>
                 </Container>
-            </Menu>
+            </Responsive>
+
+            <Responsive maxWidth={439} as={Menu}>
+                <Container>
+                    <Dropdown icon={'bars'} item>
+                        <Dropdown.Menu className={classes.menu}>
+                            <Dropdown.Item>
+                                <Link className={classes.link} to={root('/campaign')}>Campaigns</Link>
+                            </Dropdown.Item>
+                            <Dropdown.Item>
+                                <Link className={classes.link} to={root('/monsters')}>Monsters</Link>
+                            </Dropdown.Item>
+                            <Dropdown.Divider/>
+                            <Dropdown.Item>
+                                <Link className={classes.link} to={root(`/user/${id}`)}>
+                                    Profile
+                                </Link>
+                            </Dropdown.Item>
+                            <Dropdown.Item onClick={logout}>
+                                Logout
+                            </Dropdown.Item>
+                        </Dropdown.Menu>
+                    </Dropdown>
+                    <div className={classes.flex}/>
+                    <img src={icon} alt={'icon'} className={classes.logo}/>
+                    <div className={classes.flex}/>
+                    <Menu.Item className={classes.fakeItem}/>
+                </Container>
+            </Responsive>
             <Container>
                 {children}
             </Container>
