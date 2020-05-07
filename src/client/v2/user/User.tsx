68c23272284f8816eb2d7e258cc2cf45e9129c06
Matic Leva <vertical3life@gmail.com>
2020-05-07 12:44:37 +0200

    Add UserView component

--- src/client/v2/user/User.tsx
+++ src/client/v2/user/User.tsx
@@ -2,12 +2,14 @@ import React, {FC} from 'react';
 import {Route, Switch, useRouteMatch} from 'react-router';
 import {UserEdit} from './UserEdit';
 import {FourOhFour} from '../404';
+import {UserView} from './UserView';
 
 export const User: FC = () => {
     const {path} = useRouteMatch();
     return (
         <Switch>
             <Route path={`${path}/:userId/edit`} component={UserEdit}/>
+            <Route path={`${path}/:userId`} component={UserView}/>
             <Route path={'*'} component={FourOhFour}/>
         </Switch>
     );
