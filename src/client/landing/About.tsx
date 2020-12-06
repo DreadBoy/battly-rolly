8beb9220c9c247b69ccec6b0c21d7fe1b899194d
Matic Leva <vertical3life@gmail.com>
2020-12-06 22:22:06 +0100

    Update about page

--- src/client/landing/About.tsx
+++ src/client/landing/About.tsx
@@ -19,9 +19,9 @@ const useStyles = createUseStyles({
 const images = [
     ['Login or create your account', s1],
     ['Create your first campaign', s2],
-    ['Create your own monsters or subscribe to user-created ones', s3],
-    ['Create an encounter and add monsters. Click on their names and HP to edit them before or during encounter', s4],
-    ['Once ready, share your campaign to your friends and let them join. Then let the battle begin!', s5],
+    ['Create your own monsters or use existing ones', s3],
+    ['Create an encounter and add monsters. Click on their names and HP to edit them before or during the encounter', s4],
+    ['Once ready, share your campaign with your friends and let them join. Then let the battle begin!', s5],
 ]
 
 export const About: FC = () => {
@@ -31,7 +31,7 @@ export const About: FC = () => {
             <Container text className={classes.container}>
                 <Header as={'h1'}>About Crit Hit</Header>
                 <p>
-                    I play and DM D&D and I realised half of our sessions are spend communicating numbers when we could
+                    I play and GM D&D and I realised half of our sessions are spent communicating numbers when we could
                     roleplay instead. Our encounters are too long not because monsters have too much HP or we don't know
                     how to kill them efficiently, it's because we have to communicate all those numbers across the
                     table. This conversation is all too familiar in my group:
@@ -43,7 +43,7 @@ export const About: FC = () => {
                     <b>dm:</b> Noted, who's next? <br/>
                 </Segment>
                 <p>
-                    To ease the issue I created the app that is before you, Crit Hit. It was primarily designed to help
+                    To ease these issues I created the app that is before you, Crit Hit. It was primarily designed to help
                     with offline in-person play and doesn't aim to replace Roll20. You'll still have to roll your dice
                     manually and bring your own battle mat but the app will take care of dealing the damage for you so
                     you can focus on roleplay.
